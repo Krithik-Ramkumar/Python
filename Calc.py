1dddb7a9e789f5309bf0cdf25e3aa27845036fa9
Krithik-Ramkumar <krithikr914@gmail.com>
2024-08-09 10:13:44 -0400

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,27 @@
+
+import math
+
+oper_list = ["+", "-", "*", "x", "/", "**", "^"]
+
+class Calc:
+
+    def operCheck():
+        print("Invalid Syntax. Enter a valid operator from our list")
+        print(oper_list)
+        print(trig_list)
+
+
+    def add(self,x, y):
+        print(f"Answer: {x + y}")
+
+    def subtract(self,x, y):
+        print(f"Answer: {x - y}")
+    
+    def multiply(self,x, y):
+        print(f"Answer: {x * y}")
+
+    def divide(self,x, y):
+        print(f"Answer: {x / y}")
+
+    def exponent(self,x, y):
+        print(f"Answer: {x ** y}")
